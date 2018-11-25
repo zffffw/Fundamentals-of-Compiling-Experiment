e391af9febb0d4648ffc0ed7acc90be437045858
baidinga <424533148@qq.com>
2018-11-25 12:01:20 +0800

    expe3's code

---
+++ code(expe3)/myyacc.tab.h
@@ -0,0 +1,30 @@
+#ifndef YYSTYPE
+#define YYSTYPE int
+#endif
+#define	ID	258
+#define	NUMBER	259
+#define	TRUE	260
+#define	FALSE	261
+#define	IF	262
+#define	THEN	263
+#define	ELSE	264
+#define	WHILE	265
+#define	DO	266
+#define	BREAK	267
+#define	INT	268
+#define	CHAR	269
+#define	FLOAT	270
+#define	BOOL	271
+#define	RELOP_LT	272
+#define	RELOP_E	273
+#define	RELOP_GT	274
+#define	RELOP_NE	275
+#define	RELOP_EE	276
+#define	RELOP_LE	277
+#define	RELOP_GE	278
+#define	OR	279
+#define	AND	280
+#define	uminus	281
+
+
+extern YYSTYPE yylval;
